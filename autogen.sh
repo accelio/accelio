a6be3a7eb86cf5d1f8029df5ad9ce3231dc0b8d8
Eyal Salomon <esalomon@mellanox.com>
2014-02-12 21:33:25 +0200

    Adding kernel modules's autogen.sh
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ libtoolize  --force --copy --quiet  \
 && automake --gnu --add-missing \
 && autoconf 
 
-subdirlist="src/kernel/hello"
+subdirlist="src/kernel/xio src/kernel/rdma src/examples/hello_world"
 for subdir in $subdirlist ; do
 	if [ -d $subdir ]; then
 		pushd $subdir > /dev/null
