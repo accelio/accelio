01de2828c40b3e9d7ebb3bbcb1084822786af96b
Eyal Salomon <esalomon@mellanox.com>
2014-02-16 09:43:42 +0200

    correct paths in autogen.sh
    Signed-off-by: Jack Wang <jinpu.wang@profitbricks.com>
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,15 @@ libtoolize  --force --copy --quiet  \
 && automake --gnu --add-missing \
 && autoconf 
 
-subdirlist="src/kernel/xio src/kernel/rdma src/examples/hello_world"
-for subdir in $subdirlist ; do
-	if [ -d $subdir ]; then
-		pushd $subdir > /dev/null
+subdirlist=("src/kernel/xio"				\
+	    "src/kernel/rdma"				\
+	    "examples/kernel/hello_world"		\
+	    "examples/kernel/hello_world_mt")
+
+for subdir in ${!subdirlist[*]} ; do
+	#printf "	%s\n" "${subdirlist[$subdir]}"
+	if [ -d "${subdirlist[$subdir]}" ]; then
+		pushd "${subdirlist[$subdir]}"  > /dev/null
 		./autogen.sh
 		popd > /dev/null
 	fi
