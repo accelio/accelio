d549155cde82fa3c94c9c22d3e056770b3e6421f
Eyal Salomon <esalomon@mellanox.com>
2014-05-18 17:17:50 +0300

    updated script
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- tests/usr/hello_test/run_server.sh
+++ tests/usr/hello_test/run_server.sh
@@ -6,16 +6,16 @@ export LD_LIBRARY_PATH=../../../src/usr/
 server_ip=192.168.1.66
 port=1234
 core=1
-ovec=1
+ovec=0
 hdrlen=0
 
-./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 0		-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 512		-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 1024	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 4096	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 16384	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 32768	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 65536	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 131072	-l {ovec} ${server_ip}
-#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 1048576	-l {ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 0		-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 512		-l ${ovec} ${server_ip}
+./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 1024	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 4096	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 16384	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 32768	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 65536	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 131072	-l ${ovec} ${server_ip}
+#./xio_server -c ${core} -p ${port} -n ${hdrlen} -w 1048576	-l ${ovec} ${server_ip}
 
