8050840a24a3e593036a6b00f5855990fc394af1
Eyal Salomon <esalomon@mellanox.com>
2014-07-24 13:26:50 +0300

    cancel pending timer on disconnect
    Change-Id: Ic6be6bc18f473137809d835f34731e9bc4b15431
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- src/common/xio_nexus.c
+++ src/common/xio_nexus.c
@@ -1334,14 +1334,14 @@ static void xio_nexus_on_transport_disconnected(struct xio_nexus *nexus,
 {
 	int ret;
 
+	/* cancel old timers */
+	if (xio_is_delayed_work_pending(&nexus->close_time_hndl))
+		xio_ctx_del_delayed_work(nexus->transport_hndl->ctx,
+					 &nexus->close_time_hndl);
+
 	/* Try to reconnect */
 	if (g_options.reconnect) {
-		/* cancel old timers */
-		if (xio_is_delayed_work_pending(&nexus->close_time_hndl))
-			xio_ctx_del_delayed_work(nexus->transport_hndl->ctx,
-						 &nexus->close_time_hndl);
-
-		if (nexus->transport_hndl->is_client)
+			if (nexus->transport_hndl->is_client)
 			ret = xio_nexus_client_reconnect(nexus);
 		else
 			ret = xio_nexus_server_reconnect(nexus);
