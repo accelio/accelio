bcc7c594f5558b1fd4b9edd1df58dfc5529d8df6
Or Kehati <ork@mellanox.com>
2014-10-21 17:03:55 +0200

    Modify user-space hello_world example scripts to run from TOP_DIR
    Change-Id: I91c511f957887418521ab9f9157355d72cc360ed
    Signed-off-by: Or Kehati <ork@mellanox.com>

--- examples/usr/hello_world/run_server.sh
+++ examples/usr/hello_world/run_server.sh
@@ -2,6 +2,9 @@
 
 export LD_LIBRARY_PATH=../../../src/usr/
 
+# Configuring Running Directory
+TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Arguments Check
 if [ $# -lt 2 ]; then
         echo "[$0] Missing Parameters!"
@@ -16,5 +19,5 @@ if [ $# -eq 3 ]; then
 	trans=$3
 fi
 
-taskset -c 1 ./xio_server ${server_ip} ${port} ${trans}
+taskset -c 1 $TOP_DIR/xio_server ${server_ip} ${port} ${trans}
 
