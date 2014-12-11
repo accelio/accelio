4223bd3d43d6c9f9d5319306debbd7253ce1a919
Eyal Salomon <esalomon@mellanox.com>
2014-12-11 14:28:24 +0200

    Adding new error
    Change-Id: Iddb0a940b7cd860caaf40dd7e83141989344af4c
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- src/common/xio_error.c
+++ src/common/xio_error.c
@@ -124,6 +124,8 @@ static const char *xio_gen_status_str(enum xio_status ev)
 		return "User object not found";
 	case XIO_E_PEER_QUEUE_SIZE_MISMATCH:
 		return "Peer receive queue is smaller then message size";
+	case XIO_E_RSP_BUF_SIZE_MISMATCH:
+		return "Response buffer is smaller then actual response";
 	default:
 		return "Unknown error";
 	};
