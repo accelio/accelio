6b86d66c1d50f9689d2c4200ce5955bc81cf9355
Eyal Salomon <esalomon@mellanox.com>
2014-12-15 09:24:15 +0200

    running scripts modifications
    Change-Id: Id40519bcb2e5524ab1a1dd5da4fad335c3e8c891
    Signed-off-by: Eyal Salomon <esalomon@mellanox.com>

--- benchmarks/usr/xio_perftest/perf_read_bw_server.sh
+++ benchmarks/usr/xio_perftest/perf_read_bw_server.sh
@@ -9,13 +9,13 @@ if [ $# -eq 1 ]; then
 	trans=$1
 fi
 
-intf1=192.168.1.66
-intf2=192.168.1.66
-intf_name=ib0
+intf1=192.168.3.150
+intf2=192.168.3.150
+intf_name=ib2
 
 #single port server 
 #./xio_read_bw -c 8 -n 8 -r ${trans} -w "192.168.40.46:1234;192.168.40.46:1235;192.168.40.46:1236;192.168.40.46:1237;192.168.40.46:1238;192.168.40.46:1239;192.168.40.46:1240;192.168.40.46:1241" -t 100
 
 #dual port server 
-./xio_read_bw -c 8 -n 8 -r ${trans} -w "$intf1:1234;$intf2:1235;$intf1:1236;$intf2:1237;$intf1:1238;$intf2:1239;$intf1:1240;$intf2:1241" -t 100
+./xio_read_bw -c 8 -n 8 -i${intf_name} -r ${trans} -w "$intf1:1234;$intf2:1235;$intf1:1236;$intf2:1237;$intf1:1238;$intf2:1239;$intf1:1240;$intf2:1241" -t 100
 
