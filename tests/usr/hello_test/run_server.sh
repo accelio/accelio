b6805565b7a7930164ecd2ff37902840ee05af0f
Avner BenHanoch <avnerb@mellanox.com>
2015-02-10 18:37:04 +0200

    Upon failure of the test, the bash script will return success in case it found that the test was running under libfailmalloc.so and the test was terminated on error that is not related to Segmentation Violation.
    Change-Id: I4a3ab6b127f055a7c1119239eea77a42b8fbe71f

--- tests/usr/hello_test/run_server.sh
+++ tests/usr/hello_test/run_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Get Running Directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
@@ -35,5 +34,20 @@ else
 fi
 
 ./xio_server -c ${core} -p ${port} -r ${trans} -n ${hdrlen} -w ${data_len}	-l ${ovec} ${server_ip}
+RC=$?
+if [ "$RC" -eq 0 ] ; then exit 0; fi # success
 
+if [[ "$LD_PRELOAD" == *"libfailmalloc.so"* ]]
+then
+	echo "* NOTE: This test was running under libfailmalloc.so; a failure is expected, we only care whether we got SIGSEGV or not"
+	let SEGV_BASH_RC=128+11 # SEGV is 11; bash will XOR it with 128 and this will be RC of child that got SIGSEGV
+	if [ "$RC" -ne "$SEGV_BASH_RC" ]
+	then
+		echo "* SUCCESS! RC was $RC; we consider it as a success because this RC is not related to SIGSEGV (a standard failure is expected since we run under libfailmalloc.so)"
+		RC=0 # consider it as success
+	else
+		echo "* FAILURE! Child got SIGSEGV (you may consider cleaning core files)"
+	fi
+fi
+exit $RC
 
