7ca9666b609724bd0e4b3495245bdd0b157fdd09
Alon Grinshpoon <alongr@mellanox.com>
2015-12-28 15:27:06 +0200

    accelio/common: If an observer was alreday destroyed (like a transport handler in case of a reconnect) do not use nofity on him.
    Change-Id: I3b9a85ff04310cc92bfcd8e29a06b7fadca73d58

--- src/common/xio_observer.c
+++ src/common/xio_observer.c
@@ -219,7 +219,8 @@ void xio_observable_notify_all_observers(struct xio_observable *observable,
 	list_for_each_entry_safe(observer_node, tmp_observer_node,
 				 &observable->observers_list,
 				 observers_list_node) {
-		observer_node->observer->notify(
+		if(likely(observable->impl && observer_node->observer->impl))
+			observer_node->observer->notify(
 				observer_node->observer->impl,
 				observable->impl, event, event_data);
 	}
