e8253ed6bce8ad90e9df77bcbcfd041c831cafe4
Alon Grinshpoon <alongr@mellanox.com>
2016-01-05 10:35:41 +0200

    accelio/examples/usr HelloWorld example updated to use xio_mem function for big msgs
    Change-Id: I118d171360cd8216ef777071616424d48d1a874e

--- examples/usr/hello_world/xio_client.c
+++ examples/usr/hello_world/xio_client.c
@@ -38,6 +38,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
+#include <errno.h>
 
 #include "libxio.h"
 
@@ -168,16 +169,22 @@ int main(int argc, char *argv[])
 	struct xio_session_params	params;
 	struct xio_connection_params	cparams;
 	struct xio_msg			*req;
+	struct xio_reg_mem      	xbuf;
+        int 				msg_size = 0;
+        int 				max_msg_size = 0;
+        uint8_t 			*data = NULL;
 
 	if (argc < 3) {
 		printf("Usage: %s <host> <port> <transport:optional>" \
-		       "<finite run:optional>\n", argv[0]);
+		       "<finite run:optional> <msg size:optional>\n", argv[0]);
 		exit(1);
 	}
 	if (argc > 4)
 		test_disconnect = atoi(argv[4]);
 	else
 		test_disconnect = 0;
+	if (argc > 5)
+		msg_size = atoi(argv[5]);
 
 	memset(&session_data, 0, sizeof(session_data));
 	memset(&params, 0, sizeof(params));
@@ -191,6 +198,14 @@ int main(int argc, char *argv[])
 		    XIO_OPTNAME_SND_QUEUE_DEPTH_MSGS,
 		    &opt, &optlen);
 	queue_depth = QUEUE_DEPTH > opt ? opt : QUEUE_DEPTH;
+
+        /* get max msg size */
+	/* this size distinguishes between big and small msgs, where for small msgs rdma_post_send/rdma_post_recv
+	are called as opposed to to big msgs where rdma_write/rdma_read are called */
+        xio_get_opt(NULL, XIO_OPTLEVEL_ACCELIO,
+                    XIO_OPTNAME_MAX_INLINE_XIO_DATA,
+                    &opt, &optlen);
+        max_msg_size = opt;
 
 	/* create thread context for the client */
 	session_data.ctx = xio_context_create(NULL, 0, -1);
@@ -231,15 +246,29 @@ int main(int argc, char *argv[])
 		req->out.sgl_type	   = XIO_SGL_TYPE_IOV;
 		req->out.data_iov.max_nents = XIO_IOVLEN;
 
-		req->out.data_iov.sglist[0].iov_base =
-			strdup("hello world data request");
+		/* data */
+		if (msg_size < max_msg_size) { /* small msgs */
+			req->out.data_iov.sglist[0].iov_base =
+				strdup("hello world data request");
+		} else { /* big msgs */
+			if (data == NULL) {
+				printf("allocating xio memory...\n");
+				xio_mem_alloc(msg_size, &xbuf);
+				data = (uint8_t *)xbuf.addr;
+				memset(data, 0, msg_size);
+				sprintf((char *)data, "hello world data request");
+			}
+			req->out.data_iov.sglist[0].mr = xbuf.mr;
+			req->out.data_iov.sglist[0].iov_base = data;
+		}
 
-		req->out.data_iov.sglist[0].iov_len =
-			strlen((const char *)
-			  req->out.data_iov.sglist[0].iov_base)
-			   + 1;
+                req->out.data_iov.sglist[0].iov_len =
+                        strlen((const char *)
+                          req->out.data_iov.sglist[0].iov_base)
+                           + 1;
 
 		req->out.data_iov.nents = 1;
+
 		req++;
 	}
 	/* send first message */
@@ -260,9 +289,14 @@ int main(int argc, char *argv[])
 	req = session_data.req_ring;
 	for (i = 0; i < queue_depth; i++) {
 		free(req->out.header.iov_base);
-		free(req->out.data_iov.sglist[0].iov_base);
+		if (msg_size < max_msg_size) free(req->out.data_iov.sglist[0].iov_base);
 		req++;
 	}
+	if (xbuf.addr) {
+		printf("freeing xio memory...\n");
+                xio_mem_free(&xbuf);
+                xbuf.addr = NULL;
+        }
 
 	/* free the context */
 	xio_context_destroy(session_data.ctx);
