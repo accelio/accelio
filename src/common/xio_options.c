1d97fdedaed1e23efd358a283d8846d857d6b8ae
Katya Katsenelenbogen <katyak@mellanox.com>
2016-03-22 14:45:19 +0200

    accelio/common: bug fix in xio_get_opt
    Change-Id: If68ca0e52922c83c4104ee67be7dae807e1fd2ed

--- src/common/xio_options.c
+++ src/common/xio_options.c
@@ -302,7 +302,7 @@ static int xio_general_get_opt(void  *xio_obj, int optname,
 		return 0;
 	case XIO_OPTNAME_MAX_OUT_IOVLEN:
 		*optlen = sizeof(int);
-		 *((int *)optval) = g_options.max_in_iovsz;
+		 *((int *)optval) = g_options.max_out_iovsz;
 		 return 0;
 	case XIO_OPTNAME_ENABLE_RECONNECT:
 		*optlen = sizeof(int);
